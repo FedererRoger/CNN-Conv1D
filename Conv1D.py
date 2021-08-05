327b21b681be0624a3256da6683cdcfde8c091fd
FedererRoger <74909478+FedererRoger@users.noreply.github.com>
2021-08-05 13:44:45 +0300

    Update Conv1D.py

--- Conv1D.py
+++ Conv1D.py
@@ -3,6 +3,8 @@ import numpy
 from torch import nn
 import torch
 from torchvision.transforms import ToTensor
+import torch.nn.functional as F
+from torch.autograd import Variable
 
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
@@ -27,67 +29,93 @@ DataY=DataY.astype("float")
 DataX = torch.Tensor(DataX)
 DataY = torch.Tensor(DataY)
 
-print(DataX)
-
 #Определяем пользовательский датасет
 class CustomDataset(Dataset):
 
-    #def __init__(self):
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
 
     def __len__(self):
-        return len(DataY)
-    def __getitem__(self, idx):
-        image = DataX[idx]
-        label = DataY[idx]
-        return image, label
+        return len(self.Y)
 
-#Создаем экземпляр класса Dataset
-Data = CustomDataset()
-batch_size = 100
-learning_rate = 0.001
-train_loader = DataLoader(dataset=Data, batch_size=batch_size,shuffle=True)
+    def __getitem__(self, idx):
+        return self.X[idx], self.Y[idx]
+
+TT_SPLIT = 750
+train_data = CustomDataset(DataX[:TT_SPLIT,...], DataY[:TT_SPLIT])
+test_data = CustomDataset(DataX[TT_SPLIT:,...], DataY[TT_SPLIT:])
+batch_size = 200
+learning_rate = 0.003
+train_loader = DataLoader(dataset=train_data, batch_size=batch_size,shuffle=True)
+test_loader = DataLoader(dataset=test_data, batch_size=batch_size,shuffle=True)
 
 #Конструируем нейросеть
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.layer1 = nn.Sequential(nn.Conv1d(10, 16, 5), nn.ReLU(), nn.MaxPool1d(kernel_size=2, stride=2))
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(10, 20, 10),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(20, 40, 10),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),            
+        )
         self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(1720, 600),
+            nn.ReLU(),
+            nn.Linear(600, 16),
+            nn.ReLU(),
+            nn.Linear(16, 1)
+        )            
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.flatten(out)
-        return out
+        x = self.layer1(x)
+        x = self.flatten(x)
+        x = self.linear_relu_stack(x)
+        return torch.sigmoid(x)        
 
 #Обучаем модель
 model = ConvNet()
 criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-loss_list = []
-acc_list = []
-for i, (images, labels) in enumerate(train_loader):
-    # Прямой запуск
-    outputs = model(images)
-    loss = criterion(outputs, labels)
-    loss_list.append(loss.item())
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
 
-    # Обратное распространение и оптимизатор
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    # Отслеживание точности
-    total = labels.size(0)
-    _, predicted = torch.max(outputs.data, 1)
-    correct = (predicted == labels).sum().item()
-    acc_list.append(correct / total)
-
-print(acc_list)
+        if batch % 10 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct_count = 0, 0
 
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct_count += pred.eq(y.data.view_as(pred)).sum()
 
+    test_loss /= num_batches
+    correct = correct_count / size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} Correct: {correct_count} \n")
 
-
-
+epochs = 50
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_loader, model, criterion, optimizer)
+    test_loop(test_loader, model, criterion)
